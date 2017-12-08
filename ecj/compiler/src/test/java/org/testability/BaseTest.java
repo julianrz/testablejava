00c4ea458ca8163f454ed077bc36cd4dc560c4a8
julian <julianrz@yahoo.com>
2017-12-08 10:37:26 -0800

    Invoke supports varargs

--- ecj/compiler/src/test/java/org/testability/BaseTest.java
+++ ecj/compiler/src/test/java/org/testability/BaseTest.java
@@ -387,12 +387,16 @@ public class BaseTest {
         });
     }
 
-    Object invokeCompiledMethod(String className, String methodName) throws Exception {
+    Object invokeCompiledMethod(String className, String methodName, Object... args) throws Exception {
         URLClassLoader cl = new URLClassLoader(new URL[]{classStoreDir.toURL()}, this.getClass().getClassLoader());
         Class<?> clazz = cl.loadClass(className);
-        Method main = clazz.getDeclaredMethod(methodName);
-        main.setAccessible(true);
-        return main.invoke(clazz.newInstance());
+        Method[] main = clazz.getDeclaredMethods();
+
+        Optional<Method> optMethod = Arrays.stream(main).filter(m -> m.getName().equals(methodName)).findFirst();
+
+        Method method = optMethod.orElseThrow(()->new RuntimeException("method not found: " + methodName));
+        method.setAccessible(true);
+        return method.invoke(clazz.newInstance(), args);
     }
 }
 
