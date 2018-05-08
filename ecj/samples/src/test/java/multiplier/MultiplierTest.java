f14edaa6d7baa6bdcbc70cf37591e5f3971eb14a
julian <julianrz@yahoo.com>
2018-05-08 15:37:22 -0700

    Multiplier sample

---
+++ ecj/samples/src/test/java/multiplier/MultiplierTest.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2017-2018 Julian Rozentur
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package multiplier;
+
+import org.junit.Test;
+import testablejava.Helpers;
+
+public class MultiplierTest {
+    @Test(expected = NumberFormatException.class)
+    public void multiplyDoublesAsStrings() throws Exception {
+        //we know that Double.parseDouble sometimes fails and want to see how Multiplier deals with that
+        //but we do not want to invent unparsable strings
+        Multiplier multiplier = new Multiplier();
+
+        multiplier.$$Double$parseDouble$$String = (ctx, s) -> {
+            Helpers.uncheckedThrow(new NumberFormatException());
+            return null;
+        };
+
+        multiplier.multiplyDoublesAsStrings("1","2");
+    }
+
+}
